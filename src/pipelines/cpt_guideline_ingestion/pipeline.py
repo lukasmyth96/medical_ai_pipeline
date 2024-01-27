f977dcbbd36fd7a77fc0a528aa15055ccb1cf6b2
lukasmyth <lukasmyth96@gmail.com>
2024-01-27 13:20:19 +0000

    allow overwrite for guideline ingestion

--- src/pipelines/cpt_guideline_ingestion/pipeline.py
+++ src/pipelines/cpt_guideline_ingestion/pipeline.py
@@ -9,7 +9,11 @@ from pipelines.cpt_guideline_ingestion.pipeline_steps import (
 )
 
 
-def cpt_guideline_ingestion_pipeline(cpt_guideline_file_path: str | Path) -> str:
+def cpt_guideline_ingestion_pipeline(
+        cpt_guideline_file_path: str | Path,
+        cpt_code: str,
+        overwrite: bool = False,
+) -> str:
     """
     Parses guidelines and CPT code from file, creates decision tree for
     guidelines and stores both in a document in the DB.
@@ -24,7 +28,22 @@ def cpt_guideline_ingestion_pipeline(cpt_guideline_file_path: str | Path) -> str
     str:
         The ID of the created DB document.
     """
-    cpt_code = '45378'  # todo parse from PDF
+    db = Database()
+
+    existing_document = db.read(
+        collection=Collection.CPT_GUIDELINES,
+        document_id=cpt_code,
+        output_class=CPTGuidelinesDocument
+    )
+
+    if existing_document and not overwrite:
+        raise RuntimeError(
+            f"""
+            Guidelines for CPT code {cpt_code} have already been ingested.
+            Pass "overwrite=True" to the pipeline to overwrite them.
+            """
+        )
+
     cpt_guidelines = parse_cpt_guidelines_from_pdf(cpt_guideline_file_path)
     cpt_guideline_decision_tree = create_guideline_decision_tree(cpt_guidelines)
 
@@ -35,12 +54,11 @@ def cpt_guideline_ingestion_pipeline(cpt_guideline_file_path: str | Path) -> str
         decision_tree=cpt_guideline_decision_tree,
     )
 
-    db = Database()
-
     document_id = db.create(
         collection=Collection.CPT_GUIDELINES,
         document=document,
         document_id=cpt_code,
+        overwrite=overwrite,
     )
 
     return document_id
@@ -49,6 +67,12 @@ def cpt_guideline_ingestion_pipeline(cpt_guideline_file_path: str | Path) -> str
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
     file_path = input('Enter the file path for a CPT guidelines: ')
+    cpt_code = input('Enter 5 digit CPT code: ')
+    overwrite_if_exists = "y" in input("Do you want to overwrite if exists? (y/n): ").lower()
     file_path = Path(file_path)
-    assert file_path.exists()
-    cpt_guideline_ingestion_pipeline(file_path)
+    assert file_path.exists(), 'Guideline file does not exist'
+    cpt_guideline_ingestion_pipeline(
+        cpt_guideline_file_path=file_path,
+        cpt_code=cpt_code,
+        overwrite=overwrite_if_exists,
+    )
